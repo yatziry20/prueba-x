9db0252f0a87c56ba7980f2701087e085b45521e
yatziry20 <yatzialucan15@gmail.com>
2024-09-18 08:04:17 -0700

    metodo de suma

--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,17 @@ namespace Prueba_semana_1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var num1 = 7;
+            var num2 = 5;
+            int resultado = suma(num1, num2);
+        }
+
+        private int suma(int a, int b)
+        {
+            return a + b;
+        }
     }
 }
