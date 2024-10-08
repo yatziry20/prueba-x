1dcfed5123abfb31a5ba9f3127d63d891d3e5121
yatziry20 <yatzialucan15@gmail.com>
2024-10-08 16:11:44 -0700

    Add files via upload

---
+++ FundamentosPOO/FundamentosPOO/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundamentosPOO
+{
+    internal class Program
+    {
+        //TAREA 
+        //investigar las clases abstractas con ejemplo
+        static void Main(string[] args)
+        {
+            Persona2 persona2 = new Persona2("juan", "Juanito@email.com", 1);
+            Console.ReadLine();
+            persona2.Nombre = "juan juanito";
+            persona2.Email = "Juanitojuan@Email.com";
+            persona2.ImprimirNombre();
+            persona2.ImprimeEmail();
+            persona2.CambiarFecha(DateTime.Now);
+            persona2.ImprimeFecha();
+            Console.ReadLine();
+
+        }
+    }
+}
